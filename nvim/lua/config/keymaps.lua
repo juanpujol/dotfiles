94568f9f6610b4141267f691fecc4e33a3aa1920
Juan Pujol <30832+juanpujol@users.noreply.github.com>
2025-11-03 23:52:03 -0300

    Add smear-cursor plugin and line movement keybindings
    - Add smear-cursor.nvim plugin for smooth cursor animations
    - Add Alt+Shift+j/k keybindings for moving lines in nvim (works with Zellij)
    - Update nvim plugin lockfile
    - Fix Zellij layout swap keybindings (Alt+[ and Alt+])

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Move lines up/down with Alt+Shift+j/k
+vim.keymap.set("n", "<M-S-j>", "<cmd>m .+1<cr>==", { desc = "Move Line Down" })
+vim.keymap.set("n", "<M-S-k>", "<cmd>m .-2<cr>==", { desc = "Move Line Up" })
+vim.keymap.set("v", "<M-S-j>", ":m '>+1<cr>gv=gv", { desc = "Move Selection Down" })
+vim.keymap.set("v", "<M-S-k>", ":m '<-2<cr>gv=gv", { desc = "Move Selection Up" })
+vim.keymap.set("i", "<M-S-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Line Down" })
+vim.keymap.set("i", "<M-S-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Line Up" })
