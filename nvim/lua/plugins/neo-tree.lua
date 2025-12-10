c83a8bcf99c3b982bfff0e72ab8bcf17cc8e6744
Juan Pujol <30832+juanpujol@users.noreply.github.com>
2025-12-10 18:59:06 -0300

    feat(nvim): move neo-tree to right side

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,9 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    window = {
+      position = "right",
+    },
     filesystem = {
       filtered_items = {
         visible = false, -- when true, they will just be displayed differently than normal items
