3d7e139828533cb9dd67ba460d1f188af5003d2e
Juan Pujol <30832+juanpujol@users.noreply.github.com>
2026-02-21 19:05:19 -0300

    feat(nvim): add delete-without-yank keymaps for d and D

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Delete without yanking
+vim.keymap.set({ "n", "v" }, "d", '"_d', { desc = "Delete without yank" })
+vim.keymap.set({ "n", "v" }, "D", '"_D', { desc = "Delete to EOL without yank" })
+
 -- Move lines up/down with Alt+Shift+j/k
 vim.keymap.set("n", "<M-S-j>", "<cmd>m .+1<cr>==", { desc = "Move Line Down" })
 vim.keymap.set("n", "<M-S-k>", "<cmd>m .-2<cr>==", { desc = "Move Line Up" })
